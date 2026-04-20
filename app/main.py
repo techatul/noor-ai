5f0b383a8192b598a562b69cf7228774a256251b
Atul Kumar <techatul258@gmail.com>
2026-04-20 09:03:43 +0530

    Adding API key validation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Security
 from app.api.v1.endpoints.chat import router as chat_router
+from fastapi.security.api_key import APIKeyHeader
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import getAppConfig
 
 app = FastAPI()
+
+app_config = getAppConfig()
+api_key_name = app_config.APP_API_KEY_NAME
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+async def validate_api_key(key: str = Security(api_key_header)):
+    if key != api_key_name:
+        raise HTTPException(status_code=403, detail="Invalid API Key")
+    return key
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,4 +22,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(chat_router, prefix="/api/v1", tags=["api"])
+app.include_router(
+    chat_router,
+    prefix="/api/v1",
+    tags=["api"],
+    dependencies=[Depends(validate_api_key)]
+)
